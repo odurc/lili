7b56749cc9ae929b6f03a03af3266e7828c9de92
Ricardo Crudo <ricardo.crudo@gmail.com>
2016-08-23 23:54:13 +0200

    improve push pop test

--- test/01-push-pop.c
+++ test/01-push-pop.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include "lili.h"
 
+#define NODES_COUNT 100
+
 int main(void)
 {
     lili_t *list = lili_create();
+    int data[NODES_COUNT];
 
-    lili_push(list, "one");
-    lili_push(list, "two");
-    lili_push(list, "three");
-    printf("%s\n", (char *) lili_pop(list));
-    printf("%s\n", (char *) lili_pop(list));
-    printf("%s\n", (char *) lili_pop(list));
+    // lili push
+    for (int i = 0; i < NODES_COUNT; ++i)
+    {
+        data[i] = i;
+        lili_push(list, &data[i]);
+    }
+
+    // lili pop
+    for (int i = 0; i < NODES_COUNT; ++i)
+    {
+        int *value, j = NODES_COUNT - i - 1;
+        value = lili_pop(list);
+        if (*value != data[j])
+        {
+            printf("read wrong value from node (i = %i)\n", i);
+            printf("expected: %i, read: %i\n", data[i], *value);
+            return -1;
+        }
+    }
 
     lili_destroy(list);
 
