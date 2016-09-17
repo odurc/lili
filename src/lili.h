2ac1dbcb7dd757bd9cf6f4a32521dca98c5caa9d
Ricardo Crudo <ricardo.crudo@gmail.com>
2016-09-17 14:01:41 +0200

    add initial documentation

--- src/lili.h
+++ src/lili.h
@@ -57,16 +57,24 @@
 ************************************************************************************************************************
 */
 
+/**
+ * @struct node_t
+ * The node structure
+ */
 typedef struct node_t {
-    struct node_t *prev;
-    struct node_t *next;
-    void *data;
+    struct node_t *prev;    //!< pointer to previous node
+    struct node_t *next;    //!< pointer to next node
+    void *data;             //!< pointer to node data
 } node_t;
 
+/**
+ * @struct lili_t
+ * The list structure
+ */
 typedef struct lili_t {
-    int count;
-    node_t *first;
-    node_t *last;
+    int count;      //!< number of items in the list
+    node_t *first;  //!< pointer to first node of the list
+    node_t *last;   //!< pointer to last node of the list
 } lili_t;
 
 
@@ -76,24 +84,113 @@ typedef struct lili_t {
 ************************************************************************************************************************
 */
 
+/**
+ * @defgroup lili_list List Functions
+ * Set of functions to operate lists.
+ * @{
+ */
+
+/**
+ * Create a list
+ *
+ * @return pointer of a list object or NULL if memory allocation fail
+ */
 lili_t *lili_create(void);
+
+/**
+ * Destroy a list
+ *
+ * All items inside of the list will be destroyed.
+ *
+ * @param[in] list the list object
+ */
 void lili_destroy(lili_t *list);
 
+/**
+ * Push an item to list
+ *
+ * The item is pushed to the last position of the list.
+ *
+ * @param[in] list the list object
+ * @param[in] data the data pointer to be stored
+ */
 void lili_push(lili_t *list, void *data);
+
+/**
+ * Pop an item from list
+ *
+ * The last item is taken from the list.
+ *
+ * @param[in] list the list object pointer
+ *
+ * @return the data pointer of the stored item
+ */
 void *lili_pop(lili_t *list);
 
+/**
+ * Push an item to beginning of the list
+ *
+ * The item is pushed to the first position of the list.
+ *
+ * @param[in] list the list object
+ * @param[in] data the data pointer to be stored
+ */
 void lili_push_front(lili_t *list, void *data);
+
+/**
+ * Pop an item from the beginning of the list
+ *
+ * The first item is taken from the list.
+ *
+ * @param[in] list the list object pointer
+ *
+ * @return the data pointer of the stored item
+ */
 void *lili_pop_front(lili_t *list);
 
+/**
+ * Push an item to a specific position of the list
+ *
+ * The item is pushed to the position indicated by \a index,
+ * where the first position is zero and the last is list->count.
+ * The \a index might also have negative values, where -1 represents
+ * the last position, -2 the next-to-last position and so on.
+ *
+ * @param[in] list the list object
+ * @param[in] data the data pointer to be stored
+ * @param[in] index the position where to push the item
+ */
 void lili_push_at(lili_t *list, void *data, int index);
+
+/**
+ * Pop an item from a specific position of the list
+ *
+ * The item is taken from the position indicated by \a index,
+ * where the first position is zero and the last is list->count.
+ * The \a index might also have negative values, where -1 represents
+ * the last position, -2 the next-to-last position and so on.
+ *
+ * @param[in] list the list object
+ * @param[in] index the position where to push the item
+ *
+ * @return the data pointer of the stored item
+ */
 void *lili_pop_from(lili_t *list, int index);
 
+/**
+ * @}
+ */
 
 /*
 ************************************************************************************************************************
 *   CONFIGURATION ERRORS
 ************************************************************************************************************************
 */
+
+#if defined(LILI_ONLY_STATIC_ALLOCATION) && \
+  (!defined(LILI_MAX_LISTS) || !defined(LILI_MAX_NODES))
+#error "LILI_MAX_LISTS and LILI_MAX_NODES must be defined when LILI_ONLY_STATIC_ALLOCATION is defined"
+#endif
 
 
 /*
