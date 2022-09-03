ef5fa14f3cb965df9c6ed569caa0b672464b5363
Ricardo Crudo <ricardo.crudo@gmail.com>
2022-09-03 11:36:18 +0200

    Add unit test to validate push and pop functions
    Test all push and pop functions

--- test/test_lili.c
+++ test/test_lili.c
@@ -52,13 +52,13 @@
 static int setup(void **state)
 {
     static lili_t *list;
-    static int data[10];
+    static int data[5];
 
     list = lili_create();
     assert_non_null(list);
 
     // push some data to the list
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         data[i] = i;
         lili_push(list, &data[i]);
@@ -171,6 +171,79 @@ static void test_iteration(void **state)
     }
 }
 
+static bool check_list_values(lili_t *list, const int *expected)
+{
+    int i = 0;
+    LILI_FOREACH(list, node)
+    {
+        int *value = (int *) node->data;
+        if (*value != expected[i++])
+            return false;
+    }
+
+    return true;
+}
+
+static void test_pushes_and_pops(void **state)
+{
+    lili_t *list = *state;
+
+    int values[] = {10, 20, 30, 40, 50, -100};
+    int i = 0;
+
+    // push at valid position (positive value)
+    lili_push_at(list, &values[i++], 4);
+    assert_true(check_list_values(list, (const int []){0, 1, 2, 3, 10, 4}));
+
+    // push at position out of the range (positive value)
+    // value should be added at the last position instead
+    lili_push_at(list, &values[i++], 25);
+    assert_true(check_list_values(list, (const int []){0, 1, 2, 3, 10, 4, 20}));
+
+    // push at position zero, should behave as list_push_front function
+    lili_push_at(list, &values[i++], 0);
+    assert_true(check_list_values(list, (const int []){30, 0, 1, 2, 3, 10, 4, 20}));
+
+    // push at valid position (negative value)
+    lili_push_at(list, &values[i++], -2);
+    assert_true(check_list_values(list, (const int []){30, 0, 1, 2, 3, 10, 4, 40, 20}));
+
+    // push at position out of the range (negative value)
+    // value should be added at the first position instead
+    lili_push_at(list, &values[i++], -23);
+    assert_true(check_list_values(list, (const int []){50, 30, 0, 1, 2, 3, 10, 4, 40, 20}));
+
+    // push front
+    lili_push_front(list, &values[i++]);
+    assert_true(check_list_values(list, (const int []){-100, 50, 30, 0, 1, 2, 3, 10, 4, 40, 20}));
+
+    // pop front
+    lili_pop_front(list);
+    assert_true(check_list_values(list, (const int []){50, 30, 0, 1, 2, 3, 10, 4, 40, 20}));
+
+    // pop from position zero, should behave like pop_front function
+    lili_pop_from(list, 0);
+    assert_true(check_list_values(list, (const int []){30, 0, 1, 2, 3, 10, 4, 40, 20}));
+
+    // pop from valid position (negative value)
+    lili_pop_from(list, -1);
+    assert_true(check_list_values(list, (const int []){30, 0, 1, 2, 3, 10, 4, 40}));
+
+    // pop from position out of the range (positive value)
+    // value should be poped from the last position instead
+    lili_pop_from(list, 100);
+    assert_true(check_list_values(list, (const int []){30, 0, 1, 2, 3, 10, 4}));
+
+    // pop from position out of the range (negative value)
+    // value should be added at the first position instead
+    lili_pop_from(list, -50);
+    assert_true(check_list_values(list, (const int []){0, 1, 2, 3, 10, 4}));
+
+    // pop from valid position (positive value)
+    lili_pop_from(list, 4);
+    assert_true(check_list_values(list, (const int []){0, 1, 2, 3, 4}));
+}
+
 
 /*
 ****************************************************************************************************
@@ -183,6 +256,7 @@ int main(void)
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_max_config),
         cmocka_unit_test_setup_teardown(test_iteration, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_pushes_and_pops, setup, teardown),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
